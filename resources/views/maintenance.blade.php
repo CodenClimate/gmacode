4f5496536dfec201c640b4d4e1924941aee77202
Davis Too <33203305+kipkiruitoo@users.noreply.github.com>
2020-06-13 14:23:58 +0300

    finished

---
+++ resources/views/maintenance.blade.php
@@ -0,0 +1,53 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+<title>Coming Soon</title>
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+{{-- <meta name="keywords" content="Astonish Responsive, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design" /> --}}
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- font files -->
+<link href="//fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Niconne" rel="stylesheet">
+<!-- /font files -->
+<!-- css files -->
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/font-awesome.css" rel="stylesheet" type="text/css" media="all" />
+
+<!-- /css files -->
+<body>
+	<div class="w3l-banner-layer">
+	<div class="container  text-center">
+			<div class="agileinfo-logo">
+					<h1><a href="https://codenclimate.org"> <img src="./images/3.png" width="200" height="200" alt="Code n climate logo"> <br> Codenclimate</a></h1>
+			</div>
+			<h2 class="txt-w3_agile">stay tuned <br>we're launching soon. </h2>
+			<span class="bar"></span>
+			<p>We will Contact you with the email you used to signup when ready</p>
+			<div class="sub-form-wthree">
+				<form action="#" method="post">
+					<input type="email" name="email" size="30" required="" placeholder="Email">
+					<button class="btn1"><span class="fa fa-paper-plane" aria-hidden="true"></span></button>
+				</form>
+			</div>
+			<div class="agile_social_banner">
+				<ul class="social_list">
+					<li><a href="#" class="social_facebook"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
+					<li><a href="#" class="social_twitter"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
+					<li><a href="#" class="social_dribble"><span class="fa fa-dribbble" aria-hidden="true"></span></a></li>
+					<li><a href="#" class="social_vimeo"><span class="fa fa-vimeo" aria-hidden="true"></span></a></li>
+				</ul>
+			</div>
+
+	</div>
+	<div class="footer">
+		<p> &copy; 2020 . All Rights Reserved  <a href="https://codenclimate.org" target="=_blank">codenclimate.org</a></p>
+	</div>
+	</div>
+</body>
+</html>
